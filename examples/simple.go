087bfd67a8d1a27aa0f9ccb741bd1b15d77e4afe
Erich Nascimento <erichnascimento@gmail.com>
2014-10-31 00:49:58 -0200

    Add examples

---
+++ examples/simple.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+import "github.com/erichnascimento/mapdb"
+
+func main() {
+
+  // open db, loading from file if exists
+  db := mapdb.OpenDB("/tmp/mydb.db", nil)
+
+  defer db.Close()
+
+  // Set a key
+  db.Set("username", "erichnascimento")
+  db.Set("password", "1234")
+
+  // Get a key value
+  fmt.Println(db.Get("username"))
+  fmt.Println(db.Get("password"))
+
+  // Update a key
+  db.Set("username", "other-username")
+  fmt.Println(db.Get("username"))
+
+  // Delete a key
+  db.Del("password")
+  fmt.Println(db.Get("password"))
+
+  // Set a json string
+  db.Set("preferences", `{"foo": "bar"}`)
+  fmt.Println(db.Get("preferences"))
+
+  // Print all keys
+  fmt.Println(db.Keys())
+
+  // Save to file
+  db.Save()
+}
